48c1d35665eae6fe3ca08677ec4a00fd5d188bab
Mirza <kadric681@gmail.com>
2025-04-04 02:07:25 +0200

    Grid Product Button Fix

--- lib/components/Products/grid_product_component.dart
+++ lib/components/Products/grid_product_component.dart
@@ -6,86 +6,112 @@ class GridProductComponent extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return CupertinoButton(
-      pressedOpacity: 0.6,
-      onPressed: () {
-        Navigator.push(
-          context,
-          CupertinoPageRoute(builder: (context) => ProductScreen()),
-        );
-      },
-      padding: EdgeInsets.zero,
-      child: Container(
-        padding: EdgeInsets.all(12),
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(8),
-          color:
-              MediaQuery.of(context).platformBrightness == Brightness.dark
-                  ? CupertinoColors.tertiarySystemFill
-                  : CupertinoColors.white,
-          boxShadow: [
-            BoxShadow(
-              color: CupertinoColors.black.withOpacity(0.16),
-              offset: Offset(0, 4),
-              blurRadius: 12,
-            ),
-          ],
-        ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Container(
-              height: 120,
-              width: double.infinity,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(8),
-                image: DecorationImage(
-                  image: AssetImage("lib/assets/necklace.jpg"),
-                  fit: BoxFit.cover,
+    return Stack(
+      children: [
+        CupertinoButton(
+          pressedOpacity: 0.6,
+          onPressed: () {
+            Navigator.push(
+              context,
+              CupertinoPageRoute(builder: (context) => ProductScreen()),
+            );
+          },
+          padding: EdgeInsets.zero,
+          child: Stack(
+            children: [
+              Container(
+                padding: EdgeInsets.all(12),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(8),
+                  color:
+                      MediaQuery.of(context).platformBrightness ==
+                              Brightness.dark
+                          ? CupertinoColors.tertiarySystemFill
+                          : CupertinoColors.white,
+                  boxShadow: [
+                    BoxShadow(
+                      color: CupertinoColors.black.withOpacity(0.16),
+                      offset: Offset(0, 4),
+                      blurRadius: 12,
+                    ),
+                  ],
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Container(
+                      height: 120,
+                      width: double.infinity,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(8),
+                        image: DecorationImage(
+                          image: AssetImage("lib/assets/necklace.jpg"),
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 8),
+                    Text(
+                      "Lancic sa ovonekom",
+                      style: CupertinoTheme.of(
+                        context,
+                      ).textTheme.textStyle.copyWith(
+                        fontWeight: FontWeight.w600,
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                      maxLines: 1,
+                    ),
+                    Text(
+                      "Dior",
+                      style: TextStyle(
+                        color: CupertinoColors.systemGrey,
+                        fontWeight: FontWeight.w700,
+                      ),
+                    ),
+                    SizedBox(height: 4),
+                    Text(
+                      "Swarovski crystal necklace",
+                      style: TextStyle(
+                        color: CupertinoColors.systemGrey,
+                        fontSize: 14,
+                      ),
+                      maxLines: 2,
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                    SizedBox(height: 8),
+                  ],
                 ),
               ),
-            ),
-            SizedBox(height: 8),
-            Text(
-              "Lancic sa ovonekom",
-              style: CupertinoTheme.of(context)
-                  .textTheme
-                  .textStyle
-                  .copyWith(
-                    fontWeight: FontWeight.w600,
-                    overflow: TextOverflow.ellipsis,
-                  ),
-              maxLines: 1,
-            ),
-            Text(
-              "Dior",
-              style: TextStyle(
-                color: CupertinoColors.systemGrey,
-                fontWeight: FontWeight.w700,
-              ),
-            ),
-            SizedBox(height: 4),
-            Text(
-              "Swarovski crystal necklace",
-              style: TextStyle(color: CupertinoColors.systemGrey, fontSize: 14),
-              maxLines: 2,
-              overflow: TextOverflow.ellipsis,
-            ),
-            SizedBox(height: 8),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
-                  "1050 BAM",
-                  style: CupertinoTheme.of(context).textTheme.textStyle
-                      .copyWith(fontSize: 16, fontWeight: FontWeight.w600),
+
+              Positioned(
+                bottom: 16,
+                left: 16,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      "1050 BAM",
+                      style: CupertinoTheme.of(context).textTheme.textStyle
+                          .copyWith(fontSize: 16, fontWeight: FontWeight.w600),
+                    ),
+                  ],
                 ),
-                Icon(CupertinoIcons.cart_badge_plus, size: 24),
-              ],
-            ),
-          ],
+              ),
+            ],
+          ),
         ),
-      ),
+        Positioned(
+          bottom: 16,
+          right: 16,
+          child: CupertinoButton(
+            padding: EdgeInsets.zero,
+            minimumSize: const Size(0, 0),
+            onPressed: () {},
+
+            child: Icon(CupertinoIcons.cart_badge_plus, size: 24),
+          ),
+        ),
+      ],
     );
   }
 }
