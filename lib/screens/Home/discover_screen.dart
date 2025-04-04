2bbf28a1f8532636f457b7d38dea4c44e43e8417
Mirza <kadric681@gmail.com>
2025-04-04 02:02:54 +0200

    Grid Products Design Implementation

--- lib/screens/Home/discover_screen.dart
+++ lib/screens/Home/discover_screen.dart
@@ -1,3 +1,4 @@
+import 'package:aurora_jewelry/components/Products/grid_product_component.dart';
 import 'package:aurora_jewelry/widgets/profile_avatar_widget.dart';
 import 'package:flutter/cupertino.dart';
 
@@ -12,18 +13,53 @@ class DiscoverScreen extends StatelessWidget {
           CupertinoSliverNavigationBar(
             alwaysShowMiddle: false,
             middle: Text("Discover"),
-            enableBackgroundFilterBlur: false,
+        
             largeTitle: Padding(
               padding: const EdgeInsets.only(right: 16.0),
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text("Discover"),
-                  ProfileAvatarWidget()
-                ],
+                children: [Text("Discover"), ProfileAvatarWidget()],
               ),
             ),
           ),
+          SliverToBoxAdapter(
+            child: GridView.builder(
+              padding: EdgeInsets.only(left: 16, right: 16, bottom: 96),
+              shrinkWrap: true,
+              physics: NeverScrollableScrollPhysics(),
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                crossAxisSpacing: 8,
+                mainAxisSpacing: 8,
+                childAspectRatio: 0.65,
+              ),
+              itemCount: 10,
+              itemBuilder: (context, index) {
+                return GridProductComponent();
+              },
+            ),
+          ),
+          // SliverGrid(
+          //   // padding: EdgeInsets.zero,
+          //   // physics: const NeverScrollableScrollPhysics(),
+          //   gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+          //     maxCrossAxisExtent: MediaQuery.of(context).size.width / 2,
+          //     mainAxisSpacing: 200,
+          //     crossAxisSpacing: 8,
+          //     childAspectRatio: 1,
+          //   ),
+          //   delegate: SliverChildListDelegate([
+          //     GridProductComponent(),
+          //     GridProductComponent(),
+          //     GridProductComponent(),
+          //   ]),
+
+          //   // itemCount: searchProvider.categories.length,
+          //   // itemBuilder: (context, index) {
+          //   //   final category = searchProvider.categories[index];
+          //   //   return GridProductComponent();
+          //   // },
+          // ),
         ],
       ),
     );
