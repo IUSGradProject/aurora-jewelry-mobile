230ac6a0a4fa3eef9d0b2679bb24b9401a315100
Mirza <kadric681@gmail.com>
2025-05-07 10:37:31 +0200

    Fixed lint

---
+++ lib/components/Profile/previous_order_mutiple_products_component.dart
@@ -0,0 +1,117 @@
+import 'package:aurora_jewelry/models/Products/product_order_model.dart';
+import 'package:aurora_jewelry/screens/Home/Product/image_preview_screen.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+class PreviousOrderMultipleProductsComponent extends StatelessWidget {
+  final DateTime date;
+  final List<ProductOrder> items;
+
+  const PreviousOrderMultipleProductsComponent({
+    super.key,
+    required this.date,
+    required this.items,
+  });
+
+  double get total =>
+      items.fold(0, (sum, item) => sum + (item.price * item.quantity));
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(16),
+      margin: EdgeInsets.symmetric(vertical: 8),
+      decoration: BoxDecoration(
+        color: MediaQuery.of(context).platformBrightness == Brightness.dark
+            ? CupertinoColors.secondarySystemFill
+            : CupertinoColors.white,
+        border: Border.all(
+          color: MediaQuery.of(context).platformBrightness == Brightness.dark
+              ? CupertinoColors.transparent
+              : CupertinoColors.systemGrey5,
+          width: 1,
+        ),
+        borderRadius: BorderRadius.circular(16),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          // Order Date
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                DateFormat('d MMM yyyy \'at\' HH:mm').format(date),
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: CupertinoColors.systemGrey,
+                ),
+              ),
+            ],
+          ),
+          SizedBox(height: 12),
+
+          // First Product Image
+          if (items.isNotEmpty)
+            // ClipRRect(
+            //   borderRadius: BorderRadius.circular(8),
+            //   child: CupertinoButton(
+            //     padding: EdgeInsets.zero,
+            //     onPressed: () {
+            //       Navigator.of(context, rootNavigator: true).push(
+            //         CupertinoDialogRoute(
+            //           builder: (context) =>
+            //               ImagePreviewScreen(imageURL: items[0].),
+            //           context: context,
+            //         ),
+            //       );
+            //     },
+            //     child: Container(
+            //       decoration: BoxDecoration(
+            //         borderRadius: BorderRadius.circular(8),
+            //       ),
+            //       child: Image.network(
+            //         items.first.imageUrl,
+            //         width: 100,
+            //         height: 100,
+            //         fit: BoxFit.cover,
+            //       ),
+            //     ),
+            //   ),
+            // ),
+          SizedBox(height: 16),
+
+          // List of Products
+          ...items.map(
+            (item) => Padding(
+              padding: const EdgeInsets.symmetric(vertical: 4),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Expanded(child: Text("${item.name} x${item.quantity}")),
+                  Text(
+                    "${(item.price * item.quantity).toStringAsFixed(2)} BAM",
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Divider(height: 24),
+
+          // Total
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text("Total", style: TextStyle(fontWeight: FontWeight.bold)),
+              Text(
+                "${total.toStringAsFixed(2)} BAM",
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
